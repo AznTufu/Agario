da2033bb10f0aa8e97d5312d3570ac262874c6f9
Tony Zhang <zhgtony.pro@hotmail.com>
2024-05-17 10:15:41 +0200

    wip agario

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,10 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 const app = express();
 const server = http.createServer(app);
+import path from 'path';
 const PORT = 3000;
+const __dirname = path.resolve();
+//
 const io = new Server(server, {
     cors: {
         origin: '*',
@@ -15,13 +18,33 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.json('ip address: http://' + ip.address()+':'+PORT);    
 });
+//send index.html
+app.get('/game', (req, res) => {
+    //cut back to path
+    let pathIndex = __dirname.split('backend');
+    //concat path
+    let realPath = pathIndex[0]
+    console.log(realPath)
+    res.sendFile(realPath + '/front/index.html');
+});
+
+let players = {};
+let colors = ['red', 'blue', 'green', 'yellow', 'purple'];
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.broadcast.emit('user connected');
+
+    players[socket.id] = {
+        size: 4,
+        color: colors[Math.floor(Math.random() * colors.length)]
+    };
+    socket.emit('playerData', players[socket.id]);
+    
     socket.on('disconnect', () => {
         console.log('user disconnected');
         socket.broadcast.emit('user disconnected');
+        delete players[socket.id];
     });
     socket.on('message', (msg) => {
         console.log('message: ' + msg);
--- front/game.js
+++ front/game.js
@@ -1,10 +1,10 @@
-let canvas = document.getElementById('gameCanvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-let baseSpeed = 5; 
-let scale = 2;
-let border = 50;
-let colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan'];
+const baseSpeed = 5; 
+const scale = 2;
+const border = 10;
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan'];
 
 let player = {
     x: canvas.width / 2,
@@ -55,16 +55,15 @@ function checkFoodCollision() {
     for(let i = 0; i < food.length; i++) {
         let distance = Math.hypot(player.x - food[i].x, player.y - food[i].y);
         if(distance < player.size + food[i].size) {
-            player.size += food[i].size / 3;
+            player.size += food[i].size / 2;
             food.splice(i, 1);
-            for(let j = 0; j < 5; j++) {
-                let newSize = 1;
-                let newColor = colors[Math.floor(Math.random() * colors.length)];
-                food.push({x: Math.random() * (canvas.width - border * 2) + border, y: Math.random() * (canvas.height - border * 2) + border, size: newSize, color: newColor});
-            }
+            let newSize = 1;
+            let newColor = colors[Math.floor(Math.random() * colors.length)];
+            food.push({x: Math.random() * (2000 - border * 2) + border, y: Math.random() * (2000- border * 2) + border, size: newSize, color: newColor});
         }
     }
 }
+
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -72,14 +71,19 @@ function gameLoop() {
     let dy = mouse.y - player.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
 
-    let speed = baseSpeed / player.size;
+    let speed = baseSpeed / (player.size / 1.5) ;
     let scale = 10 / Math.sqrt(player.size);;
 
-    if (player.x + dx / distance * speed >= border && player.x + dx / distance * speed <= canvas.width - border) {
+    if (distance > speed) {
         player.x += dx / distance * speed;
-    }
-    if (player.y + dy / distance * speed >= border && player.y + dy / distance * speed <= canvas.height - border) {
         player.y += dy / distance * speed;
+        if (player.x < 0) player.x = 0;
+        if (player.y < 0) player.y = 0;
+        if (player.x > 2000) player.x = 2000;
+        if (player.y > 2000) player.y = 2000;
+    } else {
+        player.x = mouse.x;
+        player.y = mouse.y;
     }
 
     ctx.save();
@@ -92,17 +96,21 @@ function gameLoop() {
 
     ctx.restore();
 
+    // if (player.size >= 305) {
+    //     alert('Bien joué !');
+    // }
+
     requestAnimationFrame(gameLoop);
 }
 
-for(let i = 0; i < 5000; i++) {
+for(let i = 0; i < 1500; i++) {
     let color = colors[Math.floor(Math.random() * colors.length)];
     let size = 1;
-    let x = Math.random() * (canvas.width - border * 2) + border;
-    let y = Math.random() * (canvas.height - border * 2) + border;
+    let x = Math.random() * (2000 - border * 2) + border;
+    let y = Math.random() * (2000 - border * 2) + border;
     food.push({x: x, y: y, size: size, color: color});
 }
-    
+
 canvas.addEventListener('mousemove', function(e) {
     let rect = canvas.getBoundingClientRect();
     mouse.x = (e.clientX - rect.left) / scale + player.x - canvas.width / (2 * scale);
